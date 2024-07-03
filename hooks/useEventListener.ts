629397b0af1d2a862bfd79a0746443e7e0bc9002
Hagop Hovaguimian <hagop.microchip@gmail.com>
2024-07-03 07:13:19 +0400

    first commit

--- hooks/useEventListener.ts
+++ hooks/useEventListener.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-const useEventListener = <T extends Event>(eventName: string, handler: (event: T) => void, element: EventTarget = window) => {
+const useEventListener = <T extends Event>(eventName: string, handler: (event: T) => void, element?: EventTarget) => {
   const savedHandler = useRef<(event: T) => void>();
 
   useEffect(() => {
@@ -8,16 +8,19 @@ const useEventListener = <T extends Event>(eventName: string, handler: (event: T
   }, [handler]);
 
   useEffect(() => {
+    if (typeof window === 'undefined') return; // Skip if running on the server
+
+    const targetElement = element ?? window;
     const eventListener = (event: Event) => {
       if (savedHandler.current) {
         savedHandler.current(event as T);
       }
     };
 
-    element.addEventListener(eventName, eventListener);
+    targetElement.addEventListener(eventName, eventListener);
 
     return () => {
-      element.removeEventListener(eventName, eventListener);
+      targetElement.removeEventListener(eventName, eventListener);
     };
   }, [eventName, element]);
 };
